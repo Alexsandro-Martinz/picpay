1621fdc2f0fb2c8580ccc415181a9f0229aef830
alexsandro-martinz <martinz.alexsandro@gmail.com>
2023-11-08 22:49:41 -0300

    feat: create Profile

---
+++ api/models/profile.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Profile(models.Model):
+    class ProfileType(models.TextChoices):
+        COMMON = "COMMON"
+        MERCHANT = "MERCHANT"
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    document = models.CharField(max_length=255, unique=True)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    balance = models.DecimalField(max_digits=11, decimal_places=2)
+    profile_type = models.CharField(
+        max_length=100,
+        choices=ProfileType.choices,
+    )
+     
